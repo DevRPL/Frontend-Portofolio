88c8360dda78fcc08de23f707cbad1cf9bf3d5e9
Muhamad Jamil Firdaus <muhamadjamil779@gmail.com>
2023-06-04 09:59:59 +0700

    update footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,10 +1,22 @@
 import React from "react";
-import { MDBContainer, MDBFooter, MDBIcon, MDBRow, MDBCol } from "mdbreact";
+import { MDBContainer, MDBFooter, MDBIcon, MDBRow, MDBCol, MDBBtn } from "mdbreact";
 
 const Footer = () => {
   return (
     <MDBFooter color="primary-color-dark">
       <MDBContainer fluid className="text-center p-4">
+      <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
+        <div className='me-5 d-none d-lg-block'>
+        <span>
+            Perjalanan Sejak Tahun 2014, Luaskan Ilmu Luaskan Manfaat
+        </span>
+        </div>
+        <div>
+          <MDBBtn outline color="light" floating className='m-1' href='https://devsmk.blogspot.com/' target="_blank" role='button'>
+          <MDBIcon size="1x" icon='blog' />
+        </MDBBtn>
+        </div>
+      </section>
         <MDBRow>
           <MDBCol md="4">
             <h4 className="title pt-3 font-weight-bold">CV</h4>
@@ -25,12 +37,11 @@ const Footer = () => {
             </div>
           </MDBCol>
         </MDBRow>
-      </MDBContainer>  
-      
+      </MDBContainer>
       <div className="pt-4"></div>
       <div className="footer-copyright text-center py-3">
         <MDBContainer fluid>
-          © 2014 - { new Date().getFullYear() } Made with &hearts; using  <MDBIcon fab icon="laravel" /> <MDBIcon fab icon="react" /> 
+          © 2014 - { new Date().getFullYear() } Crafted With &hearts; In Bandung Using <MDBIcon fab icon="laravel" /> <MDBIcon fab icon="react" /> 
         </MDBContainer>
       </div>
     </MDBFooter>
