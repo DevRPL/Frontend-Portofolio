8063e5a454c759766555c5a3df4b111de8cea5fa
Muhamad Jamil Firdaus <muhamadjamil668@gmail.com>
2024-10-04 16:46:02 +0700

    update about & header image

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Header = (props) => {
     return(
-        <MDBView src="https://c.pxhere.com/photos/88/53/desktop_pen_writing_handwriting_computer_notebook_hands_desk-106505.jpg!d">
+        <MDBView src="assets/header.jpg">
               <MDBMask overlay="indigo-slight" className="flex-center flex-column text-white text-center">
                 <h2 className="h2-responsive">{props.title}</h2>
             </MDBMask>
